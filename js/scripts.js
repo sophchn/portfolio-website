377df17d8c8f33e874f879d6255270179b2be905
sophchn <sophie.chen13@gmail.com>
2021-10-28 23:04:30 -0400

    Added menuToggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Welcome to My Portfolio Site!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }

c85f3679a761bc9c777a8e1103f1830fa57eaf79
sophchn <sophie.chen13@gmail.com>
2021-11-14 23:33:47 -0500

    Updated content

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
+ /*jslint devel: true */
+ /*jslint browser */
+
 console.log("Welcome to My Portfolio Site!");
+
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
+    var x = document.getElementById("myNavtoggle");
+    if (x.className === "navtoggle") {
+        x.className += " responsive";
     } else {
-      x.className = 'navtoggle';
+        x.className = "navtoggle";
     }
-  }
+}
